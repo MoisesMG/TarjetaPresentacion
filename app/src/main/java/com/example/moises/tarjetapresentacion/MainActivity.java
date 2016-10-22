07c8815ded3bd51275f81d434d5bfb2dceccdac2
Moisés Mena <Moiss Mena>
2016-10-22 03:21:40 -0600

    botones de github y facebook implementados, mas el tamaño de la fuente aumentado

--- app/src/main/java/com/example/moises/tarjetapresentacion/MainActivity.java
+++ app/src/main/java/com/example/moises/tarjetapresentacion/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.moises.tarjetapresentacion;
 
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,8 +21,18 @@ public class MainActivity extends AppCompatActivity {
     public void onClick(View v){
         switch(v.getId()){
             case R.id.btnConoceMas:
+
                 startActivity(new Intent("com.example.moises.AboutMe"));
                 break;
+            case R.id.facebook:
+                Intent intent1 = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://www.facebook.com/menagonzalezmoises"));
+                startActivity(intent1);
+                break;
+            case R.id.github:
+                Intent intent2 = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://github.com/MoisesMG"));
+                startActivity(intent2);
             default:
                 break;
         }//fin del switch
